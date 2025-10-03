75a98a3769047d0365147eb87ffc60389240766a
stellanechi <stellanechi65@gmail.com>
2025-10-03 10:58:46 +0100

    updated product id structure

--- rendeezvous-js/DetailsPage.js
+++ rendeezvous-js/DetailsPage.js
@@ -34,18 +34,18 @@ async function renderProductDetails() {
   const product = await productService.getProductById(productId);
 
   // Display the product
-  const container = document.getElementById("product-details");
-  container.innerHTML = `
-    <div class="max-w-4xl mx-auto overflow-hidden mt-10">
-      <div class="grid grid-cols-1 md:grid-cols-2">
-        <div class="flex items-center justify-center  p-6">
-          <img src="${product.image}" alt="${product.title}" class="max-h-80 object-contain">
-        </div>
-      </div>
-    </div>
-  `;
-  //   document.getElementById("product-image").src = product.image;
-  //   document.getElementById("product-image").alt = product.title;
+  //   const container = document.getElementById("product-details");
+  //   container.innerHTML = `
+  //     <div class="max-w-4xl mx-auto overflow-hidden mt-10">
+  //       <div class="grid grid-cols-1 md:grid-cols-2">
+  //         <div class="flex items-center justify-center  p-6">
+  //           <img src="${product.image}" alt="${product.title}" class="max-h-80 object-contain">
+  //         </div>
+  //       </div>
+  //     </div>
+  //   `;
+  document.getElementById("product-image").src = product.image;
+  document.getElementById("product-image").alt = product.title;
   document.getElementById("product-title").textContent = product.title;
   document.getElementById("product-price").textContent = `$${product.price}`;
   document.getElementById("product-description").textContent =
